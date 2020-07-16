4cb1c3948ba0ea1652771a79a8722540b437d4a0
Luka Stanisavljevic <lstanisavljevic@mirabeau.nl>
2020-07-16 18:26:05 +0200

    Move URL's to env file

---
+++ src/TreningApi.js
@@ -0,0 +1,39 @@
+const { REACT_APP_TARGET_URL, REACT_APP_PROXY_URL } = process.env;
+
+const parseHtml = (str) => {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(str, "text/html");
+  return Object.values(doc.querySelectorAll("tr")).map((row) =>
+    Object.values(row.querySelectorAll("td")).map((cell) => cell.innerHTML)
+  );
+};
+const fetchSourceHtml = () => {
+  return new Promise((resolve, reject) => {
+    fetch(REACT_APP_PROXY_URL, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic",
+        "Target-URL": REACT_APP_TARGET_URL,
+      },
+      body: null,
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((responseText) => {
+        resolve(responseText);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const TreningApi = {
+  parseHtml,
+  fetchSourceHtml,
+};
+
+export default TreningApi;
